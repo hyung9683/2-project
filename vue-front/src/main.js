9c7639678fc50123e362723a9ccd8d890f475d5b
hyung9683 <sdow9178@naver.com>
2024-07-17 16:58:41 +0900

    node, vue 모듈 추가, main.js, App.vue 변경, nodemon 설치시 npm i --dev를 붙일것

--- vue-front/src/main.js
+++ vue-front/src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import Vuetify from 'vuetify';
 
-createApp(App).mount('#app')
+
+window.Kakao.init('79eb0be4a08b223b4a5553fe99835470');
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App)
+app.use(router);
+app.use(VueSweetalert2);
+app.use(Vuetify);
+app.mount('#app')
