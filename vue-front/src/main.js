fe219adac01efb7a47c8b7997c22721a2b223168
DYDY2 <doyoung1372@naver.com>
2024-07-18 12:28:47 +0900

    Revert "Merge branch 'master' into park"
    This reverts commit 13f56d2fb81a34a2b24033872a95a6164514df22, reversing
    changes made to ba24a6cb013c89a74be4443f02dc9767b6578e95.

--- vue-front/src/main.js
+++ vue-front/src/main.js
@@ -1,20 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
-import Vuetify from 'vuetify';
 
-
-window.Kakao.init('79eb0be4a08b223b4a5553fe99835470');
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-});
-
-const app = createApp(App)
-app.use(router);
-app.use(VueSweetalert2);
-app.use(Vuetify);
-app.mount('#app')
+createApp(App).mount('#app')
