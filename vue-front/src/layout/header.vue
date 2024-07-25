bb99d1685eaeef535f95ea8451376f61edc19c38
DYDY2 <doyoung1372@naver.com>
2024-07-25 12:19:45 +0900

    마이페이지, 로그인, 회원가입, 관리자 페이지

--- vue-front/src/layout/header.vue
+++ vue-front/src/layout/header.vue
@@ -1,7 +1,25 @@
 <template>
-    <div class="container">
-        
+ <!-- top navbar -->
+ <div id="top_navbar" class="col-lg-12 ms-auto bg-success-subtle fixed-top py-2 header-container" ref="header">
+    <div class="row align-items-center">
+        <div class="col-md-4">
+            <div class="nav navbar-toggle flex-colum text-light text-uppercase mb-0"><button class="btn" style="border:none;" @click="sideBarMenu"><i class="bi bi-list mx-1 me-2"></i></button><a href="#" class="nav-link text-dark logo-container" @click="goToHome"><img class="img-fluid h4-image" src="../assets/2차 프로젝트 로고2.png" style="object-fit: contain;" :style="imageSize"></a></div>
+        </div>
+        <div class="col-md-5"></div>
+        <div class="col-md-3" style="padding-left: 15%;">
+            <ul class="navbar-nav" v-if="this.user.user_no == this.loginUser.user_no">
+                <!-- <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-chat-dots text-dark bi-lg"></i></a></li> -->
+                <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-person-square text-dark bi-lg"></i></a></li>
+                <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-box-arrow-right text-danger bi-lg"></i></a></li>
+            </ul>
+            <ul class="navbar-nav" v-else>
+                <li class="nav-item"><a href="#" class="nav-link subBtn" @click="login"><div class="loginBtn">로그인</div></a></li>
+                <li class="nav-item"><a href="#" class="nav-link subBtn" @click="join"><div class="joinBtn">회원가입</div></a></li>
+            </ul>
+        </div>
     </div>
+</div>
+    <!-- /top navbar -->
 </template>
 
 <script>
@@ -10,18 +28,61 @@ export default {
     components: {},
     data() {
     return {
-        example: '',
+        imageSize: {
+            height: '7rem',
+            width: 'auto',
+
+        },
+        loginUser: {},
     }
 }, 
-    beforeCreate() {},
     created() {},
-    beforeMount() {},
     mounted() {},
-    computed: {},
-    beforeUpdate() {}, 
-    updated() {}, 
-    beforeUnmount() {},
-    unmounted() {},
-    methods: {}
+    computed: {
+        user() {
+            return this.$store.state.user;
+        }
+    },
+    methods: {
+        sideBarMenu() {
+            this.emitter.emit('SideBarMenu');
+        },
+        headerHeight() {
+            const headerHeight = this.$refs.header.offsetHeight
+            console.log(headerHeight);
+            this.emitter.emit('headerHeight', headerHeight);
+        },
+        login(){
+            this.$router.push({ path: '/login'});
+        },
+        join() {
+            this.$router.push({ path: '/join'});
+        },
+        goToHome() {
+            this.$router.push({ path: '/' });
+        }
+    }
 }
 </script>
+<style scoped>
+
+
+.logo-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 2rem;
+    overflow: hidden;
+    padding: 0;
+    
+}
+
+.h4-image {
+    height: auto;
+    max-height: 100%;
+    width: 100%;
+}
+
+
+
+</style>
--- vue-front/src/main.js
+++ vue-front/src/main.js
@@ -4,12 +4,37 @@ import { createRouter, createWebHistory } from 'vue-router'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import Vuetify from 'vuetify';
+import store from './store'
 
 //레이아웃
 import defalutlayout from './layout/defalutlayout.vue';
 
 //메인
 import Main from './view/Main.vue';
+
+import Login from './view/login.vue'
+import Join from './view/join.vue'
+import Find from './view/find.vue'
+
+import qna from './view/qna.vue'
+import qnaWrite from './view/qnaWrite.vue'
+import qnaContent from './view/qnaContent'
+
+import Qna from './admin/qna.vue'
+import QnaAns from './admin/qnaAns.vue'
+import QnaContent from './admin/qnaContent.vue'
+import UserInfo  from './admin/userinfo.vue'
+import Board  from './admin/board.vue'
+import ReportDetails from './admin/reportDetail.vue'; 
+
+import AdminLayout from './layout/adminLayout.vue'
+
+import MyPageLayout from './layout/mypageLayout'
+import MyPage from './mypage/mypage.vue'
+import myQna from './mypage/myQna.vue'
+import Contentpage from './mypage/contentpage.vue'
+import MyPageUpdate from './mypage/mypageupdate.vue'
+import Pass from './mypage/passwd.vue'
 
 const routes = [
     {
@@ -22,10 +47,91 @@ const routes = [
                 component: Main,
 
             },
-        ]
-    }
-]
-
+            {
+                path: '/login',
+                component: Login,
+            }
+            ,
+            {
+                path: '/join',
+                component: Join,
+            },
+            {
+                path: '/find',
+                component: Find,
+            },
+            {
+                path: '/qna',
+                component: qna,
+            },
+            {
+                path: '/qnawrite',
+                component: qnaWrite,
+            },
+            {
+                path: '/qna/qnacontent',
+                component:qnaContent,
+            },
+        ]},
+        {
+            path: '/admin/',
+            component: AdminLayout,
+            children: [
+                {
+                    path:'qna',
+                    component: Qna,
+                },
+                {
+                    path:'qna/qnacontent/write',
+                    component:QnaAns
+                },
+                {
+                    path:'qna/qnacontent',
+                    component:QnaContent
+                },
+                {
+                    path:'user',
+                    component:UserInfo
+                },
+                {
+                    path:'board',
+                    component:Board
+                },
+                {
+                    path:'reportdetail',
+                    component:ReportDetails
+                },
+            ]},
+            {
+        
+                path: '/mypage/',
+                name: 'mypagelayout',
+                component: MyPageLayout,
+                children: [
+                    {
+                        path: '',
+                        component: MyPage,
+                    },
+                    {
+                        path: 'qnapage',
+                        component: myQna,
+                    },
+                    {
+                        path: 'contentpage',
+                        component: Contentpage,
+                    },
+                    {
+                        path: 'update',
+                        component: MyPageUpdate,
+                    },
+                    {
+                        path: 'passwd',
+                        component: Pass,
+                    }
+                ]},
+       
+       
+    ]
 
 window.Kakao.init('79eb0be4a08b223b4a5553fe99835470');
 
@@ -36,6 +142,7 @@ const router = createRouter({
 
 const app = createApp(App)
 app.use(router);
+app.use(store);
 app.use(VueSweetalert2);
 app.use(Vuetify);
 app.mount('#app')
